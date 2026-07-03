30a409db41ea794284aaca43039fe53c24d07fff
Rudra Mehta <rudramehta1836@gmail.com>
2026-07-03 16:39:22 +0530

    created backend folders

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app = express();
+const port=process.env.PORT || 5000;
+app.get('/',(req,res)=>{
+    res.send(`Welcome to campus placement portal`);
+});
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
