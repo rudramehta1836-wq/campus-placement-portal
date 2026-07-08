fa8ba157379b7dd08398066760c98c70b7f5ca2c
Rudra Mehta <rudramehta1836@gmail.com>
2026-07-09 00:37:56 +0530

    connected database

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+const connectDB = require("./config/db");
 const express=require('express');
 const app = express();
 const port=process.env.PORT || 5000;
 app.get('/',(req,res)=>{
     res.send(`Welcome to campus placement portal`);
 });
+connectDB();
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 });
